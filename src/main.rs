60805760b7df31b2cf26bf4c853148c456c999ee
abezzi <abezzi@outlook.cl>
2023-04-04 08:33:30 -0400

    feat: read files and print the content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // when we use collect we need to specify the type of the receiver variable
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("searching for: {query}");
+    println!("in file: {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("content of the file: {contents}");
 }
