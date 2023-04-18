fe7a09ef9a0d557e14c2a512ab28cadedd4cc298
abezzi <abezzi@outlook.cl>
2023-04-17 22:08:33 -0400

    feat: modularity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use grep_clone::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -14,35 +13,9 @@ fn main() {
     });
 
     println!("searching for: {}", config.query);
-    println!("in file: {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = grep_clone::run(config) {
         println!("Applicattion error: {e}");
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("content of the file: {contents}");
-
-    Ok(())
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
 }
